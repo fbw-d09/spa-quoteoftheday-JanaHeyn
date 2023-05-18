9e1aa659b2d15f2a20ff1680c447f690b33ab8ca
JanaHeyn <jana.heyn@web.de>
2023-05-18 14:24:24 +0200

    Solve task

--- js/index.js
+++ js/index.js
@@ -14,3 +14,43 @@ const quotes = [
   ];
 
 // Write your Javascriot code here
+const button = document.getElementById('btn');
+const outputQuoteElement = document.getElementById('outputQuote');
+
+/* outputQuoteElement.addEventListener('click', (event) => {
+  document.getElementById('note').removeAttribute('hidden');
+}); */
+
+button.addEventListener('mouseover', (event) => {
+  button.style.backgroundColor = '#568e8f';
+  button.style.boxShadow = 'rgba(38, 38, 38, 0.3) 1px 5px 12px';
+  button.style.transform = 'translateY(-1px)';
+});
+
+button.addEventListener('mouseout', (event) => {
+  button.style.backgroundColor = '#5f9ea0';
+  button.style.boxShadow= 'rgba(38, 38, 38, 0.2) 1px 3px 5px';
+  button.style.transform = 'translateY(0px)';
+})
+
+button.addEventListener('mousedown', (event) => {
+  button.style.backgroundColor = '#4d7f80';
+  button.style.boxShadow = 'rgba(38, 38, 38, 0.25) 0 2px 4px';
+  button.style.transform = 'translateY(0px)';
+});
+
+button.addEventListener('mouseup', (event) => {
+  button.style.backgroundColor = '#568e8f';
+  button.style.boxShadow = 'rgba(38, 38, 38, 0.3) 1px 5px 12px';
+  button.style.transform = 'translateY(-1px)';
+})
+
+
+btn.addEventListener('click', (event) => {
+  let quotesIndex = Math.floor(Math.random() * quotes.length);
+  let randomQuote = quotes[quotesIndex];
+  outputQuoteElement.value = randomQuote;
+  
+}); 
+
+
